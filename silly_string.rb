714d3d4cfea0a10e5ba96be5c363b0be7b948ba5
aashnaag <aashna.agarwal55@gmail.com>
2018-07-23 20:02:30 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -28,7 +28,8 @@ letter_count = "supercalifragilisticexpialidocious".length
 puts "There are #{letter_count} letters in #{big_word}"
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = supercalifragilisticexpialidocious+wow+mom Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom" 
+Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
 
